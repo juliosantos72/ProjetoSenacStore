5c00f0fd34c001e3e07bdb12df0eff43ed8690b9
julio.santos <senhor.juliosantos@gmail.com>
2022-07-20 21:44:11 -0300

    melhoria no Admin

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+from Store.models import Categoria, Departamento, Produto
 
 # Register your models here.
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome']
+    list_display_links = ['id', 'nome']
+    
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'Departamento']
+    list_display_links = ['id', 'nome']
+    list_filter = ['Departamento']
+    
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'preco', 'estoque', 'categoria', 'ver_imagem']
+    list_display_links = ['id', 'nome']
+    list_filetr = ['categoria']
+    search_fields = ['nome']
+    readonly_fields = ['ver_imagem']
+    def ver_imagem(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height="{height}" />'
+         .format(
+             url = obj.imagem.url,
+             width = 75,
+             height = 75
+             )                 
+        )
+      
+admin.site.register(Departamento, DepartamentoAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Produto, ProdutoAdmin) 
