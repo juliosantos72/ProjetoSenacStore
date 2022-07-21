36ebdc4ec6acb7443f980d522261e3b5b0916c21
julio.santos <senhor.juliosantos@gmail.com>
2022-07-20 22:26:57 -0300

    Melhoria no Store

--- Store/views.py
+++ Store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HTTPResponse('Hello Word!')
+
+def teste(request):
+    return HTTPResponse('Minha pagina de teste')
